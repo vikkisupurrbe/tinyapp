9fa3c588cecda5e356c347ad3300efdf80ee60ff
Vikki <yingjialiu1216@gmail.com>
2025-03-03 20:53:32 -0800

    created a URL submission form /urls/new

--- express_server.js
+++ express_server.js
@@ -32,6 +32,10 @@ app.get("/urls", (req, res) => { // when a user visits /urls, the server
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
