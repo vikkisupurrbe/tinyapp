ac1aa020ec7bde2f7d670a19f687f6b0730eadfb
Vikki <yingjialiu1216@gmail.com>
2025-03-03 17:25:00 -0800

    set EJS as templating engine

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app =  express();
 const PORT = 8080;
 
+app.set("view engine", "ejs"); // set EJS as the templating engine
+
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
