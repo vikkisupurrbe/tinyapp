8f3235090e378b7082bc296b7feed9569630b9a3
Vikki <yingjialiu1216@gmail.com>
2025-03-04 19:29:33 -0800

    feat: add and test login

--- express_server.js
+++ express_server.js
@@ -76,3 +76,9 @@ app.post("/urls/:id", (req, res) => {
   urlDatabase[id] = newURL;
   res.redirect("/urls");
 });
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie("username", username);
+  res.redirect("/urls");
+});
