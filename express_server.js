9ce2d18361ef1d5bc0f8f2f7bba1bac870e2bb48
Vikki <yingjialiu1216@gmail.com>
2025-03-04 20:22:48 -0800

    feat: add logout route

--- express_server.js
+++ express_server.js
@@ -95,3 +95,8 @@ app.post("/login", (req, res) => {
   res.cookie("username", username);
   res.redirect("/urls");
 });
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+});
